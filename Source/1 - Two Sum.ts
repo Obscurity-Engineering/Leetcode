029ec290d9999aa254838abf3a4264deae694ded
Zachary Stanford <zachary.s1110@gmail.com>
2023-03-14 19:35:54 -0400

    Completed problem 1, added tests

--- Source/1 - Two Sum.ts
+++ Source/1 - Two Sum.ts
@@ -12,7 +12,30 @@
  * - Only one valid answer exists.
  */
 export function twoSum(values: number[], target: number): number[] {
-  values;
-  target;
+
+  // Brute Force: Nested Loops, compare every value to every value
+  // [1, 2, 3] 5 => [1, 2]
+  // [5, 0, 9, 1, 3] 6 => [0, 3]
+
+  // nums[index] = value
+  // add object with key of index and value of element to container
+  // subtract current element from target, check in container for the resulting value
+  // if not found, add current element value with its key as index
+  // one pass for loop adding elements with their index until the difference is found
+  const visited: Map<number, number> = new Map();
+  visited.set(values[0], 0);
+
+  for(let curr = 1; curr < values.length; curr++) {
+    const difference = target - values[curr]; // 5 - 3 = 2
+    const previousValue = visited.get(difference);
+
+    if(previousValue !== undefined) {
+      return [previousValue, curr];
+    } else {
+      visited.set(values[curr], curr);
+    }
+
+  }
+
   return [];
 }
